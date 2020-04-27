181325cd5bcfbc54adb4f9bf4d3ad4ab38c12789
cvvishnuu <cvishnuu01@gmail.com>
2020-04-27 20:47:29 +0530

    Face detection box done

--- src/App.js
+++ src/App.js
@@ -42,8 +42,27 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
+  }
+
+  calculateFaceLocation = (data) =>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height) 
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
@@ -52,14 +71,9 @@ class App extends Component {
 
   onDetectClick = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-        // there was an error
-      }
-  );
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
   }
 
   render() {
@@ -73,7 +87,7 @@ class App extends Component {
         onInputChange = {this.onInputChange}
         onDetectClick = {this.onDetectClick}
       />
-      <FaceDetection imageUrl = {this.state.imageUrl}/>
+      <FaceDetection box = {this.state.box} imageUrl = {this.state.imageUrl}/>
     </div>
   );
   }
--- src/components/FaceDetectionfol/FaceDetection.js
+++ src/components/FaceDetectionfol/FaceDetection.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './FaceDetection.css';
 
-const FaceDetection = ({ imageUrl }) => {
+const FaceDetection = ({ imageUrl, box }) => {
 	return (
 		<div className = 'center1'>
 			<div className = 'absolute mt2 ma2' > 
-				<img alt = '' src = {imageUrl} width = '500px' height = 'auto' />
+				<img id = 'inputimage' alt = '' src = {imageUrl} width = '500px' height = 'auto' />
+				<div className = 'bounding-box' style = {{top: box.topRow, left: box.leftCol, right: box.rightCol, bottom: box.bottomRow}}></div>
 			</div>
 		</div>
 	)
